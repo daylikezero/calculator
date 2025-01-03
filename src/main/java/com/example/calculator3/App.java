a0e1cec2c7f91b22bed52c3df847d47fb82d2a4b
yrchoi <daylikezero@gmail.com>
2025-01-03 16:15:28 +0900

    feat: calculator lv3 설계 정리

---
+++ src/main/java/com/example/calculator3/App.java
@@ -0,0 +1,12 @@
+package com.example.calculator3;
+
+/**
+ * 도전기능 Lv3. Enum, 제네릭, 람다 & 스트림을 이해한 계산기 만들기
+ * <p>
+ *     * Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리하고 이를 사칙연산 계산기 ArithmeticCalculator 클래스에 활용
+ *     * 실수, 즉 double 타입의 값을 전달 받아도 연산이 수행하도록 만들기
+ *     * 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값 들을 출력
+ * </p>
+ */
+public class App {
+}
