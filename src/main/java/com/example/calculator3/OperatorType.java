f44fc144640396015fc4d3aa007c6daeddd0eaa3
yrchoi <daylikezero@gmail.com>
2025-01-09 11:55:20 +0900

    #9 refactor: OperatorType.calculate 람다 표현식 적용

--- src/main/java/com/example/calculator3/OperatorType.java
+++ src/main/java/com/example/calculator3/OperatorType.java
@@ -3,43 +3,28 @@ package com.example.calculator3;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 /**
  * 연산자 타입
  */
 public enum OperatorType {
-    PLUS("+") {
-        @Override
-        public Number calculate(BigDecimal big1, BigDecimal big2) {
-            return big1.add(big2); // 덧셈
+    PLUS("+", BigDecimal::add),
+    MINUS("-", BigDecimal::divide),
+    MULTIPLY("*", BigDecimal::multiply),
+    DIVIDE("/", (big1, big2) -> {
+        if (big2.equals(BigDecimal.ZERO)) {
+            throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
         }
-    }, MINUS("-") {
-        @Override
-        public Number calculate(BigDecimal big1, BigDecimal big2) {
-            return big1.subtract(big2); // 뺠샘
-        }
-    }, MULTIPLY("*") {
-        @Override
-        public Number calculate(BigDecimal big1, BigDecimal big2) {
-            return big1.multiply(big2); // 곱셈
-        }
-    }, DIVIDE("/") {
-        @Override
-        public Number calculate(BigDecimal big1, BigDecimal big2) {
-            if (big2.equals(BigDecimal.ZERO)) {
-                // e2: ArithmeticException: / by zero
-                throw new ArithmeticException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-            }
-            // devide(divisor, roundingMode) : 계산결과 소수점 반올림 처리
-            return big1.divide(big2, RoundingMode.HALF_UP); // 나눗셈
-        }
-    };
+        return big1.divide(big2, RoundingMode.HALF_UP);
+    });
 
     private final String operator;
-    public abstract Number calculate(BigDecimal big1, BigDecimal big2);
+    private final BiFunction<BigDecimal, BigDecimal, Number> biFunction;
 
-    OperatorType(String operator) {
+    OperatorType(String operator, BiFunction<BigDecimal, BigDecimal, Number> biFunction) {
         this.operator = operator;
+        this.biFunction = biFunction;
     }
 
     public static Optional<OperatorType> getOperatorType(String operator) {
@@ -49,5 +34,16 @@ public enum OperatorType {
             }
         }
         return Optional.empty();
+    }
+
+
+    /**
+     * calculate() : 외부에서 Enum 상수에 정의된 biFunction을 실행할 때 사용
+     * @param num1
+     * @param num2
+     * @return apply(num1, num2)
+     */
+    public Number calculate(BigDecimal num1, BigDecimal num2) {
+        return biFunction.apply(num1, num2);
     }
 }
